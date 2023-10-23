ddef98963308ba5a6e6ca364becd52510537003e
sjwatts119 <48589467+sjwatts119@users.noreply.github.com>
2023-10-23 14:51:58 +0100

    Fix Routing and Add Home Page
    Home Page still needs formatting work

---
+++ content/home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <!-- Link BootStrap CSS Stylesheet from CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- Link Main CSS Stylesheet -->
+
+  <link rel="stylesheet" href="../content/css/home.css">
+  <!-- Google Fonts --> 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/3.0.0/flickity.css" integrity="sha512-TZTUnuHs1njGko8PJqZlHzqZTZd880A+BvhR1jy1v4mWB4VFKVLG/eK9LYdDjxqNLmttSC1ygmqg6rkYjnEgaQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+
+<body id="stop-scrolling ">
+
+    <div class="titleWrapper pt-5"><h3>Our Services</h3></div>
+    <div class="tableWrap enrolled-carousel" id="enrolled">
+
+      <div id='card' class='card'>
+        <div id='card-body' class='card-body text-center'>
+          <h5 class='card-title'>Currency Converter</h5>
+          <p class='card-text'>You can use this service to convert an Amount of One Currency to Another Currency.</p>
+          <a href='#' class='cancelEnrolmentButton btn btn-success mt-5'>Go Here</a>
+        </div> 
+      </div>
+      <div id='card' class='card text-center'>
+        <div id='card-body' class='card-body'>
+          <h5 class='card-title'>Temperature Converter</h5>
+          <p class='card-text'>You can use this service to convert a Temperature from unit Unit to Another.</p>
+          <a href='#' class='cancelEnrolmentButton btn btn-success mt-5'>Go Here</a>
+        </div> 
+      </div>
+
+    </div>
+    
+</body>
+</html>
+
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<!-- Including BootStrap JavaScript Dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- SWAL (Sweet Alerts) CDN LINK -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<!-- flickity CDN Link -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/3.0.0/flickity.pkgd.min.js" integrity="sha512-achKCfKcYJg0u0J7UDJZbtrffUwtTLQMFSn28bDJ1Xl9DWkl/6VDT3LMfVTo09V51hmnjrrOTbtg4rEgg0QArA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="../js/scripts.js"></script>
+<script rel="preconnect" src="../js/preloader.js"></script>
+<script src="../content/js/add_enrolment_form.js"></script>
+
