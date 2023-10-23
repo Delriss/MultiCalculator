e69c03945f66f5785f3d36e87225d76e2fa5a996
Finley Pettit <67767347+Delriss@users.noreply.github.com>
2023-10-23 14:04:32 +0100

    Fix Commenting

--- content/currency.php
+++ content/currency.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Currency Converter</title>
 
-  //Import BS5
+  <!--Import BS5-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
 </head>
 
@@ -13,7 +13,7 @@
   <h1>Currency Converter</h1>
 </body>
 
-//Import Scripts
+<!-- Import Scripts -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.bundle.min.js"></script>
 <script src="js/currency.js"></script>
