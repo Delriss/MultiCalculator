6e7b8f997cde18d88bc208d649f2a1eef67e7307
Finley Pettit <67767347+Delriss@users.noreply.github.com>
2023-10-23 15:15:57 +0100

    Edit to work on server

--- content/currency.php
+++ content/currency.php
@@ -6,7 +6,7 @@
   <title>Currency Converter</title>
 
   <!--Stylesheets-->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="/content/css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
 </head>
 
@@ -83,6 +83,6 @@
 <!-- Import Scripts -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.bundle.min.js"></script>
-<script src="js/script.js"></script>
+<script src="/content/js/script.js"></script>
 
 </html>
